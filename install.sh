325eefd102a9be52250e17f2a71ca912abb36ded
Austin <austinrmiller1991@gmail.com>
2017-11-30 14:28:21 -0800

    Adds more bullet proof install script

--- install.sh
+++ install.sh
@@ -1,32 +1,27 @@
 #!/bin/sh
+apt-get update
 
-install () {
-  apt-get update
+echo "Installing Docker"
+apt-get -y install docker.io
+apt-get -y install docker-ce
+docker swarm init --advertise-addr=$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
 
-  echo "Installing Docker"
-  apt-get -y install docker.io
-  apt-get -y install docker-ce
-  docker swarm init --advertise-addr=$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+echo "Installing Git"
+apt-get -y install git
 
-  echo "Installing Git"
-  apt-get -y install git
+DIRECTORY="prometheus-grafana-alertmanager-example"
+if [ -d "$DIRECTORY" ]; then
+  rm -rf "$DIRECTORY"
+fi
+echo "Cloning Project"
+git clone https://github.com/PagerTree/prometheus-grafana-alertmanager-example.git
+cd "$DIRECTORY"
 
-  DIRECTORY="prometheus-grafana-alertmanager-example"
-  if [ -d "$DIRECTORY" ]; then
-    rm -rf "$DIRECTORY"
-  fi
-  echo "Cloning Project"
-  git clone https://github.com/PagerTree/prometheus-grafana-alertmanager-example.git
-  cd "$DIRECTORY"
+echo "Making Utility scripts executable"
+chmod +x ./util/*.sh
 
-  echo "Making Utility scripts executable"
-  chmod +x ./util/*.sh
-
-  echo "Starting Application"
-  docker stack deploy -c docker-compose.yml prom
-  echo "Waiting 30 seconds for services to come up"
-  sleep 30
-  ./util/status.sh
-}
-
-install
+echo "Starting Application"
+docker stack deploy -c docker-compose.yml prom
+echo "Waiting 30 seconds for services to come up"
+sleep 30
+./util/status.sh
