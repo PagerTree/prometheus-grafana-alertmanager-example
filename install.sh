2ec59142ad8184674c54bf8528d4726028499bb8
Austin <austinrmiller1991@gmail.com>
2017-11-30 14:05:06 -0800

    Adds more bullet proof install script

--- install.sh
+++ install.sh
@@ -7,13 +7,13 @@ install () {
   docker swarm init --advertise-addr=$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
 
   apt-get install git-all
-  git clone https://github.com/PagerTree/prometheus-grafana-alertmanager-example.git
 
   DIRECTORY="prometheus-grafana-alertmanager-example"
   if [ -d "$DIRECTORY" ]; then
-    rm -rf $DIRECTORY
+    rm -rf "$DIRECTORY"
   fi
-  cd $DIRECTORY
+  cd "$DIRECTORY"
+  git clone https://github.com/PagerTree/prometheus-grafana-alertmanager-example.git
 
   chmod +x ./util/*.sh
   ./util/start.sh
